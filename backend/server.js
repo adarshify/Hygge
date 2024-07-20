55daf8f16889d7c90e0c0d051f6b4d67e22bac88
Adarsh <adarshchourasia6996@gmail.com>
2024-07-21 01:12:02 +0530

    building Hygge

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+
+
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+
+app.get ("/", (req,res)=>{
+    res.send("Kold Wrld");   
+});
+
+
+app.use("/api/auth", authRoutes) 
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
