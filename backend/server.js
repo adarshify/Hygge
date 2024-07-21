5a9981a7fa07aaade2a24a9c75c8de47dadfa023
Adarsh <adarshchourasia6996@gmail.com>
2024-07-22 03:44:06 +0530

    added signup

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+
 dotenv.config();
-const PORT = process.env.PORT || 3000;
 
-
-app.get ("/", (req,res)=>{
-    res.send("Kold Wrld");   
-});
-
-
+app.use(express.json());  //to parse the incoming requset with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
 
 
+
+
+//app.get ("/", (req,res)=>{
+ //   res.send("Kold Wrld");   
+//  });
+
+
+
+
+
 app.listen(PORT, ()=>{
+    connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
 });
