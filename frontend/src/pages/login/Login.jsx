3af635679e64df89b3b6cd9f437f1aa81392d24c
adarshify <adarshchourasia6996@gmail.com>
2024-08-29 03:36:34 +0530

    buliding login page

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,39 @@
+
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full  p-6 rounded-lg  shadow-md bg-gray-400 bg-clip-padding backdrop-filter  backdrop-blur-3xl bg-opacity-50">
+    <h1 className="text-3xl font-semibold text-center text-gray-500">
+      Login
+      <span className=" text-[#f8f7f8]"> HYGGE</span>
+    </h1>
+
+    <form >
+      <div>
+        <label className=" label p-2">
+          <span className=" text-[#f8f7f8] label-text font-bold ">Username</span>
+        </label>
+        <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+      </div>
+
+      <div>
+        <label className="label">
+          <span className="text-[#f8f7f8] label-text font-bold ">Password</span>
+        </label>
+
+          <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+        <a href="#" className="text-sm text-[#f8f7f8] font-bold mt-2 inline-block">{"Don't"} have an account</a>
+
+        <div>
+          <button className="btn btn-block btn-sm mt-2 text-[#f8f7f8] font-bolf">Login</button>
+        </div>
+      </div>
+    </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
